bb2b295d3a6a110c45f747bc1e31204de9d8a860
a2lex7ey <a2lex7ey@yandex.ru>
2025-08-11 21:45:48 +0300

    начало вёрстки index.blade.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -31,5 +33,10 @@ class Book extends Model
     public function genre(): BelongsTo
     {
         return $this->belongsTo(Genre::class);
+    }
+
+    public  function scopeExistBooks(Builder $query): void
+    {
+        $query->where('count', '>', 0)->get();
     }
 }
